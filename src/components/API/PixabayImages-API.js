bed9db111a01f7f838c37cb79daebabdb5373468
Lesya-Brodskaya <110602282+Lesya-Brodskaya@users.noreply.github.com>
2023-04-25 18:39:25 +0300

    changes in API

--- src/components/API/PixabayImages-API.js
+++ src/components/API/PixabayImages-API.js
@@ -4,7 +4,7 @@ const BASE_URL = 'https://pixabay.com/api';
 const KEY = '34292986-668a020bed047eca08aa180e6';
 
 async function fetchImages(query, page) {
-  const url = `${BASE_URL}?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&page=${page}&per_page=12`;
+  const url = `${BASE_URL}/?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&page=${page}&per_page=12`;
 
   try {
     const response = await axios.get(url);
